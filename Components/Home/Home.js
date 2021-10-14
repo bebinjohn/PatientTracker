a388f7cf98af91fd9924adae5a23e6e545c73cd2
bhavyadamo <bhavyadamo25@gmail.com>
2021-10-14 17:29:30 +0530

    hello

--- Components/Home/Home.js
+++ Components/Home/Home.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import {View,Button} from 'react-native'
+import {Text, View,Button} from 'react-native'
 import Login from './Login'
 import Signup from './signup'
 export default function Home({navigation}) {
     return (
        <View style={{margin:80}}>
+          <Text>Hello</Text>
           <Login/>
           <Signup/>
           <Button onPress={()=>navigation.navigate('Patient')} title="Go to patients"/>
